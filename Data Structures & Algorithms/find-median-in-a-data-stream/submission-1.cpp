b6dc06dc4f205021c9a0deca29357fd61804b1ea
Adarsh Shankar Rai <154750409+Adarshrai24@users.noreply.github.com>
2026-07-21 10:15:59 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.cpp
@@ -0,0 +1,30 @@
+class MedianFinder {
+public:
+    //max heap
+    priority_queue<int> pq1;
+    //min heap
+    priority_queue<int, vector<int>, greater<int>> pq2;
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        pq1.push(num);
+        int temp = pq1.top();
+        pq1.pop();
+        pq2.push(temp);
+        if (pq2.size() > pq1.size()) {
+            temp = pq2.top();
+            pq2.pop();
+            pq1.push(temp);
+        }
+    }
+    
+    double findMedian() {
+        if (pq1.size() != pq2.size()) {
+            return (double)pq1.top();
+        } else {
+            return (double)(pq1.top() + pq2.top()) / 2.0;
+        }
+    }
+};
